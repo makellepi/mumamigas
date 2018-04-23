4300a815b42771ed18fefbff8c7dc0c46785494e
Marta Keller <marta.keller.pie@gmail.com>
2018-04-23 15:35:01 +0100

    search psg

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,20 +2,25 @@ class LocationsController < ApplicationController
    before_action :set_user
 
   def index
-    @locations = Location.all
+    if params[:query].present?
+      @locations = Location.where(title: params[:query])
+    else
+      @locations = Location.all
+    end
+  end
+
+  def show
+    if params[:query].present?
+      @locations = Location.where(title: params[:query])
+    else
+      @locations = Location.all
+    end
   end
 
   def new
     @location = Location.new
   end
 
-  def show
-    if params[:query].present?
-      @locations = Location.where(city: params[:query])
-    else
-      @locations = Location.all
-    end
-  end
 
   def create
     @location = Location.new(location_params)
