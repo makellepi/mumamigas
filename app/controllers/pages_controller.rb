69632f42bfb85dd02d1dd8685e97e319e4fc6660
Marta Keller <marta.keller.pie@gmail.com>
2018-05-21 14:20:08 +0100

    DRY code on search page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,5 +5,12 @@ class PagesController < ApplicationController
   end
 
   def search
+      if params[:query].present?
+        @locations = Location.where(city: params[:query])
+        @interests = Interest.where(category: params[:query])
+      else
+        @locations = Location.all
+        @interests = Interest.all
+      end
   end
 end
