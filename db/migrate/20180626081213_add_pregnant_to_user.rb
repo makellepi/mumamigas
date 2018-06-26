334c5315378d76509873bf0dbb9c18ca0298c14f
Pedro Marzagao <pedromarzagao@gmail.com>
2018-06-26 09:19:06 +0100

    added pregnant and pregnantmonths columns to Users

--- db/migrate/20180626081213_add_pregnant_to_user.rb
+++ db/migrate/20180626081213_add_pregnant_to_user.rb
@@ -1,6 +1,6 @@
 class AddPregnantToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :pregnant, :boleean
+    add_column :users, :pregnant, :boolean
     add_column :users, :pregnantmonths, :integer
   end
 end
