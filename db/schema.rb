c8d0312708c5d2abf769c7e35d678ba8046234ec
Pedro Marzagao <pedromarzagao@gmail.com>
2018-07-07 19:05:29 +0100

    Created table Conversations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180705133057) do
+ActiveRecord::Schema.define(version: 20180707180408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20180705133057) do
     t.datetime "updated_at", null: false
     t.string "location"
     t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "receiver_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "follows", force: :cascade do |t|
