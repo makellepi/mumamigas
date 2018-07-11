79fef4f2a270ddb3d71b542477a313b7da78429f
Marta Keller <marta.keller.pie@gmail.com>
2018-07-11 11:56:31 +0100

    added image as cache with mount uploader

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -3,6 +3,6 @@ class Activity < ApplicationRecord
   validates :date, presence: true
   validates :time, presence: true
 
-  mount_uploader :photo, PhotoUploader
+  mount_uploader :image, PhotoUploader
 
 end
