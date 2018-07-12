26e13003924e9621d455f4cb0930fac0a4472f91
Pedro Marzagao <pedromarzagao@gmail.com>
2018-07-13 00:32:10 +0100

    Created models and necessary files through migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180712184008) do
+ActiveRecord::Schema.define(version: 20180712233128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,20 @@ ActiveRecord::Schema.define(version: 20180712184008) do
     t.string "sender"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "notifications", force: :cascade do |t|
+    t.string "target_type"
+    t.bigint "target_id"
+    t.string "object_type"
+    t.bigint "object_id"
+    t.boolean "read", default: false, null: false
+    t.text "metadata"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["object_type", "object_id"], name: "index_notifications_on_object_type_and_object_id"
+    t.index ["read"], name: "index_notifications_on_read"
+    t.index ["target_type", "target_id"], name: "index_notifications_on_target_type_and_target_id"
   end
 
   create_table "pg_search_documents", force: :cascade do |t|
